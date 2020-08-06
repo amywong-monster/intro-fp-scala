a33df27a8bcd395c7a5378532e7c07474e338961
Amy Wong <amy.wong@monster.com>
2020-08-06 12:42:12 +1000

    Add EitherUsage

---
+++ src/main/scala/examples/EitherUsage.scala
@@ -0,0 +1,17 @@
+package examples
+
+object EitherUsage {
+  sealed trait Party
+  case object Independent extends Party
+  case object Socialist extends Party
+  case object Liberal extends Party
+
+  def func(name: String, eligibleParty: Either[String, Party]): String =
+    eligibleParty match {
+      case Left(err) => s"$name is not eligible for election because $err"
+      case Right(Socialist) => s"$name is leftist"
+      case Right(Liberal) => s"$name is conservative"
+      case Right(Independent) => s"$name does not belong to any party"
+    }
+
+}
