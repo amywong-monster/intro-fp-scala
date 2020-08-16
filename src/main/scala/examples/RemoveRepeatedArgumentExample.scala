82488cde2b89b607dc70b07d683fba9595d1a45d
Amy Wong <amy.wong@monster.com>
2020-08-17 00:18:29 +1000

    Simplify `createUserDetails`

--- src/main/scala/examples/RemoveRepeatedArgumentExample.scala
+++ src/main/scala/examples/RemoveRepeatedArgumentExample.scala
@@ -5,46 +5,38 @@ import examples.Types.{ Configuration, Result1, Result2, Result3, UserDetail }
 object RemoveRepeatedArgumentExample {
   case class Reader[A](run: Configuration => A) {
     def flatMap[B](f: A => Reader[B]): Reader[B] =
-      Reader { cfg =>
-        f(run(cfg)).run(cfg)
-      }
+      Reader(cfg => f(run(cfg)).run(cfg))
 
     def map[B](f: A => B): Reader[B] =
-      flatMap { a =>
-        Reader(_ => f(a))
-      }
+      flatMap(a => Reader(_ => f(a)))
   }
 
   def createResult1: Reader[Result1] =
-    Reader { cfg =>
-      Result1(s"user details from ${cfg.endpoint1}")
-    }
+    Reader(cfg => Result1(s"user details from ${cfg.endpoint1}"))
 
   def createResult2(r1: Result1): Reader[Result2] =
-    Reader { cfg =>
-      Result2(s"user details from ${cfg.endpoint2} and $r1")
-    }
+    Reader(cfg => Result2(s"user details from ${cfg.endpoint2} and $r1"))
 
   def createResult3(r1: Result1): Reader[Result3] =
-    Reader { cfg =>
-      Result3(s"user details from ${cfg.endpoint3} and $r1")
-    }
+    Reader(cfg => Result3(s"user details from ${cfg.endpoint3} and $r1"))
 
-  def createUserDetails: Reader[UserDetail] =
+  def createUserDetails(r1: Result1, r2: Result2, r3: Result3): Reader[UserDetail] =
+    Reader(cfg => UserDetail(s"Simon PJ found from ${cfg.dbUrl}", r1, r2, r3))
+
+  val finalReader: Reader[UserDetail] =
     for {
-      r1  <- createResult1
-      r2  <- createResult2(r1)
-      r3  <- createResult3(r1)
-      cfg <- Reader[Configuration](run = cfg => cfg)
-      dbUrl = cfg.dbUrl
-      userName = s"Simon PJ found from $dbUrl" // suppose the username obtain from from dbUrl
-    } yield UserDetail(userName, r1, r2, r3)
+      r1         <- createResult1
+      r2         <- createResult2(r1)
+      r3         <- createResult3(r1)
+      userDetail <- createUserDetails(r1, r2, r3)
+    } yield userDetail
 
-//  scala> RemoveRepeatedArgumentExample.createUserDetails.run(Types.cfg)
-// val res13: examples.Types.UserDetail = UserDetail(Simon PJ found from
+  val result = finalReader.run(Types.cfg)
+
+//  scala> RemoveRepeatedArgumentExample.result
+// val res3: examples.Types.UserDetail = UserDetail(Simon PJ found from
   // jdbc:postgresql://localhost:5432/postgres,Result1(user details from
   // http://localhost:8080/url1),Result2(user details from http://localhost:8080/url2 and Result1(user details
   // from http://localhost:8080/url1)),Result3(user details from http://localhost:8080/url3 and Result1(user
   // details from http://localhost:8080/url1)))
-
 }
--- src/main/scala/examples/RepeatedArgumentExample.scala
+++ src/main/scala/examples/RepeatedArgumentExample.scala
@@ -12,18 +12,18 @@ object RepeatedArgumentExample {
   def createResult3(cfg: Configuration, r1: Result1): Result3 =
     Result3(s"user details from ${cfg.endpoint3} and $r1")
 
-  def createUserDetails(cfg: Configuration): UserDetail = {
-    val r1 = createResult1(cfg)
-    val r2 = createResult2(cfg, r1)
-    val r3 = createResult3(cfg, r1)
-
-    val dbUrl = cfg.dbUrl
-    val userName = s"Simon PJ found from $dbUrl" // suppose the username obtain from from dbUrl
+  def createUserDetails(cfg: Configuration, r1: Result1, r2: Result2, r3: Result3): UserDetail = {
+    val userName = s"Simon PJ found from ${cfg.dbUrl}" // suppose the username obtain from from dbUrl
     UserDetail(userName, r1, r2, r3)
   }
 
-//  scala> RemoveRepeatedArgumentExample.createUserDetails.run(Types.cfg)
-// val res1: examples.Types.UserDetail = UserDetail(Simon PJ found from
+  val r1 = createResult1(Types.cfg)
+  val r2 = createResult2(Types.cfg, r1)
+  val r3 = createResult3(Types.cfg, r1)
+  val result = createUserDetails(Types.cfg, r1, r2, r3)
+
+//  scala> RepeatedArgumentExample.result
+// val res0: examples.Types.UserDetail = UserDetail(Simon PJ found from
   // jdbc:postgresql://localhost:5432/postgres,Result1(user details from
   // http://localhost:8080/url1),Result2(user details from http://localhost:8080/url2 and Result1(user details
   // from http://localhost:8080/url1)),Result3(user details from http://localhost:8080/url3 and Result1(user
