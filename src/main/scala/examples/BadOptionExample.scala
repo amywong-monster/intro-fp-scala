292dc7c363ffe39a612cb88be89ae505f1bf6d05
jinilover <columbawong@gmail.com>
2021-07-15 11:14:39 +1000

    Modify data structure

--- src/main/scala/examples/BadOptionExample.scala
+++ src/main/scala/examples/BadOptionExample.scala
@@ -6,24 +6,33 @@ object BadOptionExample {
   case object PartitionByMonth extends Partition
   case object PartitionByYear extends Partition
 
-  def partitionPeriod(p: Partition, month: Option[String], year: Option[String]): String =
+  def partitionPeriod(
+    p: Partition,
+    month: Option[String],
+    year: Option[Int],
+    leapYearCheck: Option[Boolean]
+  ): String =
     p match {
-      case NoPartition      => "There is no partition"
-      case PartitionByMonth => s"Partition by ${month.get} month"
-      case PartitionByYear  => s"Partition by ${year.get} year"
+      case NoPartition                          => "There is no partition"
+      case PartitionByMonth                     => s"Partition by ${month.get} month"
+      case PartitionByYear if leapYearCheck.get =>
+        val v = year.get
+        val leap = if (v % 4 == 0) "leap " else ""
+        s"Partition by $leap year $v"
+      case PartitionByYear                      => s"Partition by year ${year.get}"
     }
 
   def exampleCall(): String = {
-    partitionPeriod(NoPartition, None, None)
-    partitionPeriod(PartitionByMonth, None, None)
+    partitionPeriod(NoPartition, None, None, None)
+    partitionPeriod(PartitionByMonth, None, None, None)
   }
 
-//  scala> partitionPeriod(NoPartition, None, None)
+//  scala> partitionPeriod(NoPartition, None, None, None)
 //  val res2: String = There is no partition
 
-//  scala> partitionPeriod(PartitionByMonth, Some("Jan"), None)
+//  scala> partitionPeriod(PartitionByMonth, Some("Jan"), None, None)
 //  val res10: String = Partition by Jan month
 
-//  scala> partitionPeriod(PartitionByYear, None, Some("2020"))
-//  val res11: String = Partition by 2020 year
+//  scala> partitionPeriod(PartitionByYear, None, Some(2020), Some(false))
+//  val res11: String = Partition by year 2020
 }
--- src/main/scala/examples/FixToOptionProblem.scala
+++ src/main/scala/examples/FixToOptionProblem.scala
@@ -4,13 +4,16 @@ object FixToOptionProblem {
   sealed trait Partition
   case object NoPartition extends Partition
   case class PartitionByMonth(month: String) extends Partition
-  case class PartitionByYear(year: String) extends Partition
+  case class PartitionByYear(year: Int, leapCheck: Boolean) extends Partition
 
   def partitionPeriod(p: Partition): String =
     p match {
-      case NoPartition             => "There is no partition"
-      case PartitionByMonth(month) => s"Partition by ${month} month"
-      case PartitionByYear(year)   => s"Partition by ${year} year"
+      case NoPartition                  => "There is no partition"
+      case PartitionByMonth(month)      => s"Partition by ${month} month"
+      case PartitionByYear(year, false) => s"Partition by ${year} year"
+      case PartitionByYear(year, _)     =>
+        val leap = if (year % 4 == 0) "leap " else ""
+        s"Partition by $leap $year year"
     }
 
   partitionPeriod(NoPartition)
